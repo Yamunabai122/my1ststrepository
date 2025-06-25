07b6b79b4dd6f86352f1d7baa9cc18dfb440a243
Yamunabai122 <gugulothyamunabai24@gmail.com>
2025-06-25 15:17:40 +0530

    Add files via upload

---
+++ Dom.js
@@ -0,0 +1,11 @@
+// DOM in javascript
+document.title = "DOM in JavaScript";
+document.body.style.backgroundColor = "Blue";
+document.getElementById("Mrcet").innerHTML = "HELLO, World!";
+document.getElementryById("mr").style.color = "blue";
+
+document.getElementById("mr").style.fontSize = "30px";
+
+document.getElementsByClassName("ds")[1].style.color ="red";
+
+
